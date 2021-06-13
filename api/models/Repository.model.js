29f0b50c76b4c01d2680e7ae588e237aaa8dcc63
Bea Avis <bea.avis.p@gmail.com>
2021-06-13 23:07:43 +0200

    Created repo model

---
+++ api/models/Repository.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const repoSchema = new Schema(
+  {
+    githubID: { type: Number, required: true },
+    node_id: { type: String, required: true },
+    name: { type: String, required: true },
+    full_name: { type: String, required: true },
+    private: { type: Boolean, required: true },
+    created_at: { type: Date, required: true },
+    updated_at: { type: Date},
+    pushed_at: { type: Date},
+    size: { type: Number},
+    stargazers_count: { type: Number},
+    watchers_count: { type: Number},
+    forks_count: { type: Number},
+    open_issues_count: { type: Number},
+    default_branch: { type: String},
+    network_count: { type: Number},
+    subscribers_count: { type: Number},
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = doc._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
+
+const Repository = mongoose.model('Repository', repoSchema);
+module.exports = Repository;
