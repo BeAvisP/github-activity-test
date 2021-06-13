4a52461e8e3c67acda2d63b0f7f477b8f5642917
Bea Avis <bea.avis.p@gmail.com>
2021-06-13 21:09:03 +0200

    Update app.js routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router';
+import { Toaster } from 'react-hot-toast';
 import './App.css';
+import OrganizationListPage from './pages/OrganizationListPage/OrganizationListPage';
+import RepositoryDetailsPage from './pages/RepositoryDetailsPage/RepositoryDetailsPage';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Toaster />
+      </div>
+      <Switch>
+        <Route exact path='/' component={OrganizationListPage} />
+        <Route exact path='/:org' component={OrganizationListPage} />
+        <Route exact path='/:org/:repo' component={RepositoryDetailsPage} />
+      </Switch>
     </div>
   );
 }
