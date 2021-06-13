e6b4f522e46da25489df4b1d39fbed36f62f9796
Bea Avis <bea.avis.p@gmail.com>
2021-06-13 20:58:44 +0200

    Added basic theme variables

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const fonts = {
+  base: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif',
+};
+
+export const colors = {
+  primary: '#013A63',
+  secondary: '#A9D6E5',
+  top: '#f72585',
+  background: '#fdfffc',
+};
